64b8ee02e22e3e985332728c97f5c749ae366ae5
GCreative850 <gccreative850@gmail.com>
2026-07-11 17:23:37 -0500

    Cache backend music frontend

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'zztv-static-v11';
-const ASSETS = ['./', './index.html', './styles.css', './app-stable.js', './manifest.webmanifest'];
+const CACHE_NAME = 'zztv-static-v12';
+const ASSETS = ['./', './index.html', './styles.css', './app-backend.js', './manifest.webmanifest'];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
